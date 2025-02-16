c08853f75c454f5331d9761906d9a1ba7fcb5481
VaishnaviTyagi-123 <vaishtyagi123@gmail.com>
2025-02-16 23:47:12 +0530

    Solution to #Q17

---
+++ Bitwise_Swap two numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int A,B;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+    A=A^B;
+    B=A^B;
+    A=A^B;
+    
+    printf("%d\n", A);
+    printf("%d", B);
+    return 0;
+}
