cc22ec7ba68c04fc4cd789748830ef3f2eb42794
VaishnaviTyagi-123 <vaishtyagi123@gmail.com>
2025-02-28 23:18:11 +0530

    Solution to #Q36

---
+++ Bitwise_Check for Multiple of 4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int n,result;
+    scanf("%d", &n);
+    result=n&3;
+    if(result==0){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+    return 0;
+}
